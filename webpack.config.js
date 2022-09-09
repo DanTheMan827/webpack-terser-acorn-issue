3cb600a784edabd7e7b965be7a94d182b2b63739
Daniel <790119+DanTheMan827@users.noreply.github.com>
2022-09-09 15:48:13 -0500

    Remove unused stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,11 @@
 const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const internalPath = path.resolve(path.join(__dirname, 'node_modules', '.cache', 'dist-temp'));
 
-
-const babelLoader = {
-  loader: "babel-loader",
-  options: {
-    presets: [
-      '@babel/typescript',
-      '@babel/preset-env'
-    ],
-    plugins: ['@babel/plugin-transform-runtime', '@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
-    cacheDirectory: true
-  }
-};
-
 jsEntries = {
   main: './src/index.js',
-}
-
-styleEntries = {
-  style: './stylesheets/main.scss'
 }
 
 module.exports = (env, argv) => {
@@ -34,16 +16,8 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
-          test: /\.(?:js|ts)x?$/,
-          use: [
-            babelLoader
-          ],
-          exclude: /(node_modules|bower_components)/
-        },
-        {
           test: /\.(?:pug|jade)$/,
           use: [
-            babelLoader,
             'pug-loader'
           ]
         },
@@ -65,20 +39,7 @@ module.exports = (env, argv) => {
         template: './src/templates/index.pug',
         minify: false
       }),
-      new webpack.DefinePlugin((() => {
-        const mode = argv.mode || "development"
-
-        return {
-          '__DEVELOPMENT__': mode === 'development',
-          '__PRODUCTION__': mode === 'production'
-        }
-      })())
     ],
-    resolve: {
-      extensions: [
-        '.wasm', '.mjs', '.ts', '.tsx', '.js', '.jsx', '.json'
-      ]
-    },
     output: {
       path: internalPath,
       filename: '[contenthash].js',
